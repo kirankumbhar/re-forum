474c60abd5be34e118db8493e2b1d41b9cffb881
Kiran Kumbhar <kiran.kumbhar@protegrity.com>
2019-08-21 11:47:35 +0530

    loading skeleton on post details page

--- src/containers/PostDetails/PostDetails.js
+++ src/containers/PostDetails/PostDetails.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
-import { Icon } from 'antd';
+import { Icon, Skeleton, Card, Avatar } from 'antd';
 
 import axios from '../../axios';
 
+const { Meta } = Card;
+
 class PostDetails extends Component {
     state = {
+        loading: true,
         postData: null,
         error: false
     }
@@ -14,24 +17,44 @@ class PostDetails extends Component {
             this.setState({
                 ...this.state,
                 postData: response.data,
-                error: false
+                error: false,
+                loading: false
             });
         }).catch(error => {
             
             this.setState({
                 ...this.state,
-                error: true
+                error: true,
+                loading: false
             });
         });
         
     }
  render() {
-     let post = <Icon type="loading" />;
+     let post = (
+         <Card
+         style={{ width: 600, marginTop: 16 }}
+         actions={[
+             <Icon type="setting" key="setting" />,
+             <Icon type="edit" key="edit" />
+            ]}
+            >
+        <Skeleton loading={this.state.loading} avatar active>
+            <h2>{this.state.loading === false ? this.state.postData.title : null}</h2>
+          <Meta
+            avatar={
+                <Avatar>JD</Avatar>
+            }
+            title={this.state.loading === false ? this.state.postData.author : null}
+            />
+          <p>{this.state.loading === false ? this.state.postData.description : null}</p>
+        </Skeleton>
+      </Card>
+     )
      if (this.state.postData) {
-         post = <p> Post loaded! </p>
-     }
-     if (this.state.error) {
-         post = <p> There was some error while loading post! </p>
+        }
+        if (this.state.error) {
+            post = <p> There was some problem loading Topic. Please try again later! </p>
      }
      return (<section>
          {post}
