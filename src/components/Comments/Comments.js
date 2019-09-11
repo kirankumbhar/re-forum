3d429b1b48576649cfb2f720e92ac1526719817a
Kiran Kumbhar <kiran.kumbhar@protegrity.com>
2019-09-11 11:29:19 +0530

    Implement CommentForm component

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -1,24 +1,48 @@
 import React from 'react';
-import { Comment, Avatar } from 'antd';
+import { Comment, Avatar, Tooltip, Icon } from 'antd';
 
-
+//dummy comment data
+let comments = [];
+for (let i = 0; i < 6; i++) {
+  comments.push({
+    id: i,
+    comment: "This is comment on the lorem ipsum post Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Senectus et netus et malesuada. Duis ultricies lacus sed turpis tincidunt. Pellentesque sit amet porttitor eget dolor. Quam nulla porttitor massa id neque. Lacus sed turpis tincidunt id aliquet risus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at.",
+    author: "John Doe",
+    likes: "4",
+    last_modified_at: "a day ago"
+  });
+}
 
 const Comments = (props) => {
+  return comments.map((comment) => { 
     return (
         <Comment
-    actions={[<span key="comment-nested-reply-to">Reply to</span>]}
-    author={<a>John Doe</a>}
-    avatar={
-      <Avatar>JD</Avatar>
-    }
-    content={
-      <p>
-        This is comment text
-      </p>
-    }
-  >
-  </Comment>
+          actions={[
+            <span key="comment-basic-like">
+              <Tooltip title="Like">
+                <Icon
+                  type="like"
+                  theme='outlined'
+                  onClick={comment.like}
+                />
+              </Tooltip>
+              <span style={{ paddingLeft: 8, cursor: 'auto' }}>{comment.likes}</span>
+            </span>,
+            <span key="comment-nested-reply-to">Reply to</span>
+          ]}
+          author={<a>{comment.author}</a>}
+          avatar={
+            <Avatar>JD</Avatar>
+          }
+          content={
+            <p>
+              {comment.comment}
+            </p>
+          }
+        >
+        </Comment>
     );
+  });
 }
 
 export default Comments;
--- src/containers/PostDetails/PostDetails.js
+++ src/containers/PostDetails/PostDetails.js
@@ -5,22 +5,8 @@ import axios from '../../axios';
 import { getUsernameInitials } from '../../utils';
 import FullPost from '../../components/FullPost/FullPost';
 import Comments from '../../components/Comments/Comments';
+import CommentForm from '../../components/Comments/CommentForm/CommentForm';
 import styles from './PostDetails.module.css';
-
-const { TextArea } = Input;
-
-const CommentForm = (props) => (
-    <div>
-        <Form.Item>
-            <TextArea rows={4} onChange={props.onChange} value={props.value} />
-        </Form.Item>
-        <Form.Item>
-            <Button htmlType="submit" loading={props.loading} onClick={props.onClick} type="primary">
-                Add Comment
-            </Button>
-        </Form.Item>
-    </div>
-);
 
 class PostDetails extends Component {
     state = {
@@ -47,9 +33,6 @@ class PostDetails extends Component {
 
     }
     render() {
-        getUsernameInitials('Kiran');
-        getUsernameInitials('Kiran Kumbhar');
-        getUsernameInitials('Kiran P Kumbhar');
         let post = <FullPost postData={this.state.postData} loading={this.state.loading} />;
         if (this.state.error) {
             post = <p> There was some problem loading Topic. Please try again later! </p>
@@ -59,14 +42,7 @@ class PostDetails extends Component {
                 {post}
                 <Card className={styles.CommentsCard}>
                 <section className={styles.AddComment}>
-                    <Comment
-                        avatar = {
-                            <Avatar>JD</Avatar>
-                        }
-                        content = {
-                            <CommentForm/>
-                        }
-                    />
+                    <CommentForm />
                 </section>
                 <Comments/>
                 </Card>
