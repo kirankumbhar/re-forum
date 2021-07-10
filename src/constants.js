6614865e033736f70a1d5c8587b4ea45229f7e81
Kiran Kumbhar <kiranpk189@gmail.com>
2021-07-10 18:57:28 +0530

    add comment to the post

--- src/constants.js
+++ src/constants.js
@@ -1,14 +1,16 @@
+export const POST_ID = process.env.REACT_APP_POST_ID || "post_id"
 export const POST_TITLE = process.env.REACT_APP_POST_TITLE || "title"
 export const POST_AUTHOR = process.env.REACT_APP_POST_AUTHOR || "author"
 export const POST_DESCRIPTION = process.env.REACT_APP_POST_DESCRIPTION || "description"
 export const POST_LIKES = process.env.REACT_APP_POST_LIKES || "likes"
 export const POST_VIEWS = process.env.REACT_APP_POST_VIEWS || "views"
 
-export const COMMENT_BODY = process.env.REACT_APP_COMMENT_BODY
-export const COMMENT_AUTHOR = process.env.REACT_APP_COMMENT_AUTHOR
-export const COMMENT_LIKES = process.env.REACT_APP_COMMENT_LIKES
-export const COMMENT_LAST_UPDATED = process.env.REACT_APP_COMMENT_LAST_UPDATED
-export const PARENT_COMMENT_ID = process.env.REACT_APP_PARENT_COMMENT_ID
+export const COMMENT_BODY = process.env.REACT_APP_COMMENT_BODY || "comment_body"
+export const COMMENT_AUTHOR = process.env.REACT_APP_COMMENT_AUTHOR || "author_name"
+export const COMMENT_AUTHOR_ID = process.env.REACT_APP_COMMENT_AUTHOR || "author"
+export const COMMENT_LIKES = process.env.REACT_APP_COMMENT_LIKES || "likes"
+export const COMMENT_LAST_UPDATED = process.env.REACT_APP_COMMENT_LAST_UPDATED || "last_updated"
+export const PARENT_COMMENT_ID = process.env.REACT_APP_PARENT_COMMENT_ID || "parent_comment_id"
 
 export const FIRST_NAME = process.env.REACT_APP_FIRST_NAME
 export const LAST_NAME = process.env.REACT_APP_LAST_NAME
