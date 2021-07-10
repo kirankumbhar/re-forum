cab0f58652b6a575240f15795613d4a84b48a2a1
Kiran Kumbhar <kiranpk189@gmail.com>
2021-07-11 00:03:10 +0530

    WIP: UI revamp of Post Details page

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,9 +1,9 @@
 import React, { Fragment } from 'react';
 import { Skeleton, Card, Avatar } from 'antd';
+import { LikeOutlined } from "@ant-design/icons";
 
 import styles from './FullPost.module.css';
 import { getUsernameInitials } from '../../utils';
-import ActionItems from '../ActionItems/ActionItems';
 
 import * as constants from '../../../src/constants'
 
@@ -13,29 +13,36 @@ const fullPost = (props) => {
     if (props.postData) {
         post = (
             <Fragment>
-                <h2>{props.postData[constants.POST_TITLE]}</h2>
-                <Meta
+                {/* <Meta
                     avatar={
-                        <Avatar>{getUsernameInitials(props.postData[constants.POST_AUTHOR])}</Avatar>
+                        <Avatar size={22}>{getUsernameInitials(props.postData[constants.POST_AUTHOR])}</Avatar>
                     }
                     title={props.postData[constants.POST_AUTHOR]}
-                />
-                <div className={styles.Description} dangerouslySetInnerHTML={{__html: props.postData.description}}/>
+                /> */}
+                <div className={styles.AvatarHead}>
+                    <Avatar>{getUsernameInitials(props.postData[constants.POST_AUTHOR])}</Avatar>
+                    <span className={styles.AuthorName}>{props.postData[constants.POST_AUTHOR]}</span>
+                </div>
+                <h2>{props.postData[constants.POST_TITLE]}</h2>
+                <div className={styles.Description} dangerouslySetInnerHTML={{ __html: props.postData.description }} />
             </Fragment>
         );
     }
     return (
         <Fragment>
-            <Card style={{ marginTop: 16 }}>
+            <Card className={styles.Post}>
                 <Skeleton loading={props.loading} avatar paragraph={{ rows: 6 }}>
                     {post}
                 </Skeleton>
-                <ActionItems
-                    likeCount = {props.postData ? props.postData[constants.POST_LIKES] : 0 }
-                    viewCount = {props.postData ? props.postData[constants.POST_VIEWS] : 0 }
+                <div className={styles.ActionItems}>
+                    <LikeOutlined className={styles.Icon} twoToneColor="#1890ff" />
+                </div>
+                {/* <ActionItems
+                    likeCount={props.postData ? props.postData[constants.POST_LIKES] : 0}
+                    viewCount={props.postData ? props.postData[constants.POST_VIEWS] : 0}
                     liked={props.liked}
                     postLikeHandler={props.postLikeHandler}
-                />
+                /> */}
             </Card>
         </Fragment>
     );
